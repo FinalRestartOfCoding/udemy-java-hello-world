e9ee190683f013fe88170eb7fb70b38168a70a4d
FinalRestartOfCoding <finalrestartofcoding@gmail.com>
2022-10-27 19:05:17 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    // Java looks for this method when we run this program.
+    public static void main(String[] args) {
+
+        // Prints Hello world! to the console.
+        System.out.println("Hello world!");
+    }
+}
